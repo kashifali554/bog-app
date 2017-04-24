d3bef3784b2f8504c766d193c677c7fd4bc62859
Kashif Ali <kashifali874@gmail.com>
2017-04-23 22:42:52 -0700

    html end-points are finished

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,6 +1,36 @@
 class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
-    render :index
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+    # create a new creature from `creature_params`
+    creature = Creature.new(creature_params)
+
+    # if creature saves, redirect to route that displays all creatures
+    if creature.save
+      redirect_to creatures_path
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
+  end
+
+  def show
+    creature_id = params[:id]
+    @creature_id = Creature.find_by_id(creature_id)
+  end
+
+
+  private
+
+  def creature_params
+    # whitelist params return whitelisted version
+    params.require(:creature).permit(:name, :description)
   end
 end
